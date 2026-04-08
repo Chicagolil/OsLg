b23b65dd9b4b2d52b73c99358dadbc5fdbb6fd67
LeDestructeur3000 <he202305@students.ephec.be>
2026-04-08 16:13:48 +0200

    q3

--- binary_buddy.h
+++ binary_buddy.h
@@ -59,7 +59,7 @@ typedef struct {
     int *alloc_level;
 
     // Question 3
-    size_t *ptrs;
+    int *ptrs;
 
 
 } Allocator; // Cannot modify the name of this struct, but you can add fields to it
